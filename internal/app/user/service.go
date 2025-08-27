7d5bf89cec5613ea7f4af90afbbdc30b81b889a4
mengxin756 <2379853740@qq.com>
2025-08-27 10:47:34 +0800

    第一次可以运行的项目

--- internal/app/user/service.go
+++ internal/app/user/service.go
@@ -1,45 +1,43 @@
 package user
 
 import (
-    "context"
+	"context"
 )
 
 type Service interface {
-    Get(ctx context.Context, id int) (*DTO, error)
-    Create(ctx context.Context, in CreateInput) (*DTO, error)
+	Get(ctx context.Context, id int) (*DTO, error)
+	Create(ctx context.Context, in CreateInput) (*DTO, error)
 }
 
 type CreateInput struct {
-    Name  string `json:"name"`
-    Email string `json:"email"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 type DTO struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Email string `json:"email"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 type service struct {
-    repo Repository
+	repo Repository
 }
 
 func NewService(repo Repository) Service { return &service{repo: repo} }
 
 func (s *service) Get(ctx context.Context, id int) (*DTO, error) {
-    u, err := s.repo.Get(ctx, id)
-    if err != nil {
-        return nil, err
-    }
-    return &DTO{ID: u.ID, Name: u.Name, Email: u.Email}, nil
+	u, err := s.repo.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &DTO{ID: u.ID, Name: u.Name, Email: u.Email}, nil
 }
 
 func (s *service) Create(ctx context.Context, in CreateInput) (*DTO, error) {
-    u, err := s.repo.Create(ctx, in.Name, in.Email)
-    if err != nil {
-        return nil, err
-    }
-    return &DTO{ID: u.ID, Name: u.Name, Email: u.Email}, nil
+	u, err := s.repo.Create(ctx, in.Name, in.Email)
+	if err != nil {
+		return nil, err
+	}
+	return &DTO{ID: u.ID, Name: u.Name, Email: u.Email}, nil
 }
-
-
